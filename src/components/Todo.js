9f6fdac34f239618b040ef0f49cde19ace623174
jefftdb <jefftdb@gmail.com>
2024-09-05 21:07:23 -0300

    Aula 05/09/2024

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,13 +3,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare } from '@fortawesome/free-regular-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-export const Todo = ({task}) => {
+export const Todo = ({task,toggleComplite, deleteTodo, editTodo}) => {
   return (
     <div className='Todo'>
-      <p>{task.task}</p>
-      <div>
-        <FontAwesomeIcon icon={faPenToSquare} />
-        <FontAwesomeIcon icon={faTrash} />
+      <p onClick={()=> toggleComplite(task.id)} className = {`${task.complited ? "completed" : ""}`}>{task.task}</p>
+            
+    <div>
+        <FontAwesomeIcon icon={faPenToSquare} onClick={()=> editTodo(task.id)} />
+        <FontAwesomeIcon icon={faTrash} onClick={()=> deleteTodo(task.id)} />
       </div>
     </div>
   )
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -8,14 +8,29 @@ export const TodoWrapper = () => {
   const addTodo = todo =>{
     setTodos([...todos,{id: uuidv4(),task:todo, completed:false, isEditing:false}])
   }
+  const toggleComplite = id =>{
+    setTodos(todos.map(todo => todo.id === id ? {...todo,completed: !todo.completed}: todo))
+  }
+
+  const deleteTodo = id =>{
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+  const editTodo = id =>{
+    setTodos(todos.map(todo => todo.id === id ? {...todo,isEditing: !todo.isEditing}: todo))
+  }
   return (
     <div className='TodoWrapper'>
       <h1>Bora Estudar!</h1>
       <TodoForm  addTodo={addTodo} />
 
-      {todos.map(todo, index =>(
-        <Todo task={todo} key={index} />
+      { todos.map((todo,index) =>(
+        <Todo task={todo} key={index}
+        toggleComplite={toggleComplite}
+        deleteTodo={deleteTodo}
+        editTodo={editTodo} />
       ))}
     </div>
   )
+
+
 }
